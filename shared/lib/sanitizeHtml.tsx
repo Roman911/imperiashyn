ba0a4eb0ddfb655ad26717b7d014205b214b638d
romanlysyk <roma-lysyk@ukr.net>
2026-02-13 21:38:42 +0200

    Replace `isomorphic-dompurify` with `dompurify` for client-side sanitization. Update `HtmlContent` component to use hooks for state management and align sanitization logic accordingly. Update dependency versions in `package.json` and `package-lock.json`.

--- shared/lib/sanitizeHtml.tsx
+++ shared/lib/sanitizeHtml.tsx
@@ -1,14 +1,39 @@
-import DOMPurify from 'isomorphic-dompurify';
+'use client';
 
-export const HtmlContent = ({ htmlString, className }: { htmlString: string, className?: string }) => {
-	const sanitizedHtml = DOMPurify.sanitize(htmlString, {
-		ADD_TAGS: [ 'iframe' ],
-		ADD_ATTR: [ 'allow', 'allowfullscreen', 'frameborder', 'scrolling', 'loading', 'referrerpolicy' ]
-	});
+import { useEffect, useState } from 'react';
+import createDOMPurify from 'dompurify';
+
+export const HtmlContent = ({
+															htmlString,
+															className,
+														}: {
+	htmlString: string;
+	className?: string;
+}) => {
+	const [sanitizedHtml, setSanitizedHtml] = useState('');
+
+	useEffect(() => {
+		const DOMPurify = createDOMPurify(window);
+
+		const clean = DOMPurify.sanitize(htmlString, {
+			ADD_TAGS: ['iframe'],
+			ADD_ATTR: [
+				'allow',
+				'allowfullscreen',
+				'frameborder',
+				'scrolling',
+				'loading',
+				'referrerpolicy',
+			],
+		});
+
+		setSanitizedHtml(clean);
+	}, [htmlString]);
+
 	return (
 		<div
-			className={ className }
-			dangerouslySetInnerHTML={ { __html: sanitizedHtml } }
+			className={className}
+			dangerouslySetInnerHTML={{ __html: sanitizedHtml }}
 		/>
 	);
 };
